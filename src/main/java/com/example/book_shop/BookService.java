ed0816832a6978b1c8e0f4e92df6dc9b549c15e3
RaufAliev789 <www.rauf09@gmail.com>
2026-07-20 18:51:30 +0300

    Added method 'searchAllBooksByFilter' in  1| BookService

--- src/main/java/com/example/book_shop/BookService.java
+++ src/main/java/com/example/book_shop/BookService.java
@@ -2,7 +2,10 @@ package com.example.book_shop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -16,5 +19,24 @@ public class BookService {
         this.bookMapper = bookMapper;
     }
 
+    public List<Book> searchAllBooksByFilter(Long authorId, BookSearchFilter filter){
+        int pageSize = filter.pageSize() != null
+                ? filter.pageSize() : 10;
+        int pageNumber = filter.pageNumber() != null
+                ? filter.pageNumber() : 0;
+
+        var pageable = Pageable
+                .ofSize(pageSize)
+                .withPage(pageNumber);
+
+        List<BookEntity> allEntities = repository.searchAllByFilter(
+                authorId,
+                pageable
+                );
+
+        return allEntities.stream()
+                .map(mapper :: toDomain)
+                .toList();
+    }
 
 }
