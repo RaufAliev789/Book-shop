f451a09c79588c174feffc0139ba420ea11a52f7
RaufAliev789 <www.rauf09@gmail.com>
2026-07-20 18:52:47 +0300

    Added method 'searchAllByFilter' (for method searchAllBookByFilter) in  1| BookRepository

--- src/main/java/com/example/book_shop/BookRepository.java
+++ src/main/java/com/example/book_shop/BookRepository.java
@@ -1,7 +1,22 @@
 package com.example.book_shop;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    @Query(value = """
+            SELECT b FROM BookEntity b
+            JOIN b.story s
+            JOIN s.author a
+            WHERE (a.authorId = :authorId)
+            """)
+    List<BookEntity> searchAllByFilter(
+            @Param("authorId") Long authorId,
+            Pageable pageable
+    );
 
 }
